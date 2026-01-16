ef799a0f0084caf54c0d6db020297839d34c4635
guhnhyeokko <guhnhyeokko@gmail.com>
2026-01-16 08:22:40 +0000

    Deploying to gh-pages from @ guhnhyeokko/guhnhyeokko.github.io@caf1bca96992df71dd37b8e5984de1ec01b3d0f8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -80,16 +80,8 @@ ninja.data = [{
           description: "~~",
           section: "Members",handler: () => {
               window.location.href = "/members/seungju-jeong/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-homepage-launch-we-are-excited-to-announce-the-launch-of-our-new-homepage-please-take-a-look-around-to-learn-more-about-our-research-member-and-projects",
+          title: 'Homepage Launch - We are excited to announce the launch of our new...',
           description: "",
           section: "News",},{id: "projects-biomedical-ultrasound",
           title: 'Biomedical Ultrasound',
